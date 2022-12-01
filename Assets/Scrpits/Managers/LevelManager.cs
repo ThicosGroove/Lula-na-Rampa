605409454f13e80f759696cc7c08223d028634f7
ThicosGroove <thiagottlsilva@gmail.com>
2022-12-01 15:25:44 -0300

    Pneu Obstacle implemented better

--- Assets/Scrpits/Managers/LevelManager.cs
+++ Assets/Scrpits/Managers/LevelManager.cs
@@ -110,7 +110,6 @@ public class LevelManager : Singleton<LevelManager>
         switch (currentLevel)
         {
             case 1:
-                Debug.LogWarning("Level 1");
                 current_obstacleSpeed = obstacleSpeed_Level_1;
                 current_obstacleSpawnDelay = obstacleSpawnDelay_Level_1;
                 current_collectableSpawnDelay = collectableDelay_Level_1;
@@ -120,11 +119,9 @@ public class LevelManager : Singleton<LevelManager>
                 ScoreEvents.OnChangeLevel(((int)CurrentLevelState.LEVEL_1));
                 break;
             case 2:
-                Debug.LogWarning("Level 2");
                 ScoreEvents.OnChangeLevel(((int)CurrentLevelState.LEVEL_2));
                 while (current_obstacleSpeed <= obstacleSpeed_Level_2 - 0.1f)
                 {
-                    Debug.LogWarning("LERPING 2");
                     current_obstacleSpeed = Mathf.Lerp(obstacleSpeed_Level_1, obstacleSpeed_Level_2, (Time.time - timeToStart) * lerpToNextLevel);
                     current_obstacleSpawnDelay = Mathf.Lerp(obstacleSpawnDelay_Level_1, obstacleSpawnDelay_Level_2, (Time.time - timeToStart) * lerpToNextLevel);
                     current_collectableSpawnDelay = Mathf.Lerp(collectableDelay_Level_1, collectableDelay_Level_2, (Time.time - timeToStart) * lerpToNextLevel);
@@ -133,14 +130,11 @@ public class LevelManager : Singleton<LevelManager>
                     current_playerRollingSpeed = Mathf.Lerp(playerRollingDelay_Level_1, playerRollingDelay_Level_2, (Time.time - timeToStart) * lerpToNextLevel);
                     yield return null;
                 }
-                Debug.LogWarning("SAiu lerp level 2");
                 break;
             case 3:
-                Debug.LogWarning("Level 3");
                 ScoreEvents.OnChangeLevel(((int)CurrentLevelState.LEVEL_3));
                 while (current_obstacleSpeed <= obstacleSpeed_Level_3 - 0.1f)
                 {
-                    Debug.LogWarning("LERPING 3");
                     current_obstacleSpeed = Mathf.Lerp(obstacleSpeed_Level_2, obstacleSpeed_Level_3, (Time.time - timeToStart) * lerpToNextLevel);
                     current_obstacleSpawnDelay = Mathf.Lerp(obstacleSpawnDelay_Level_2, obstacleSpawnDelay_Level_3, (Time.time - timeToStart) * lerpToNextLevel);
                     current_collectableSpawnDelay = Mathf.Lerp(collectableDelay_Level_2, collectableDelay_Level_3, (Time.time - timeToStart) * lerpToNextLevel);
@@ -149,14 +143,11 @@ public class LevelManager : Singleton<LevelManager>
                     current_playerRollingSpeed = Mathf.Lerp(playerRollingDelay_Level_2, playerRollingDelay_Level_3, (Time.time - timeToStart) * lerpToNextLevel);
                     yield return null;
                 }
-                Debug.LogWarning("SAiu lerp level 3");
                 break;
             case 4:
-                Debug.LogWarning("Level 4");
                 ScoreEvents.OnChangeLevel(((int)CurrentLevelState.LEVEL_4));
                 while (current_obstacleSpeed <= obstacleSpeed_Level_4 - 0.1f)
                 {
-                    Debug.LogWarning("LERPING 4");
                     current_obstacleSpeed = Mathf.Lerp(obstacleSpeed_Level_3, obstacleSpeed_Level_4, (Time.time - timeToStart) * lerpToNextLevel);
                     current_obstacleSpawnDelay = Mathf.Lerp(obstacleSpawnDelay_Level_3, obstacleSpawnDelay_Level_4, (Time.time - timeToStart) * lerpToNextLevel);
                     current_collectableSpawnDelay = Mathf.Lerp(collectableDelay_Level_3, collectableDelay_Level_4, (Time.time - timeToStart) * lerpToNextLevel);
@@ -165,14 +156,11 @@ public class LevelManager : Singleton<LevelManager>
                     current_playerRollingSpeed = Mathf.Lerp(playerRollingDelay_Level_3, playerRollingDelay_Level_4, (Time.time - timeToStart) * lerpToNextLevel);
                     yield return null;
                 }
-                Debug.LogWarning("SAiu lerp level 4");
                 break;
             case 5:
-                Debug.LogWarning("Level 5");
                 ScoreEvents.OnChangeLevel(((int)CurrentLevelState.LEVEL_5));
                 while (current_obstacleSpeed <= obstacleSpeed_Level_5 - 0.1f)
                 {
-                    Debug.LogWarning("LERPING 5");
                     current_obstacleSpeed = Mathf.Lerp(obstacleSpeed_Level_4, obstacleSpeed_Level_5, (Time.time - timeToStart) * lerpToNextLevel);
                     current_obstacleSpawnDelay = Mathf.Lerp(obstacleSpawnDelay_Level_4, obstacleSpawnDelay_Level_5, (Time.time - timeToStart) * lerpToNextLevel);
                     current_collectableSpawnDelay = Mathf.Lerp(collectableDelay_Level_4, collectableDelay_Level_5, (Time.time - timeToStart) * lerpToNextLevel);
@@ -181,7 +169,6 @@ public class LevelManager : Singleton<LevelManager>
                     current_playerRollingSpeed = Mathf.Lerp(playerRollingDelay_Level_4, playerRollingDelay_Level_5, (Time.time - timeToStart) * lerpToNextLevel);
                     yield return null;
                 }
-                Debug.LogWarning("SAiu lerp level 5");
                 break;
             default:
                 break;
